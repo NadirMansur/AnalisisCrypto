beeab15d58faa4294181031b51a66c72a056382a
NadirMansur <nadirmansur89@gmail.com>
2023-03-10 13:33:19 -0300

    se organizo un poco mejor el proyecto

--- Obtencion de ETH.js
+++ Obtencion de ETH.js
@@ -1,6 +1,7 @@
 'use strict'
 let valorCrypto = {};
 let ethData;
+let ethDataArray = [];
 
 function getEthPrice(){
 const apiKey = 'f742c3d3-4ebf-4a5d-b030-8db6ad804a3a';
@@ -16,22 +17,37 @@ const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?s
 
     .then(response => response.json())
     .then(data => {
-        ethData = data.data.ETH;
+        ethData = {
+            ...data.data.ETH,
+            date: new Date().toISOString() // Obtener la fecha y hora actual en formato ISO string
+        };
         return ethData;
     });
 }
 
 // EJECUCION DE GET PRICE.
-getEthPrice().then(data => {
-   ethData = data;
+//for(let i=0; i<3;i++){
+    getEthPrice().then(data => {
+    ethData = data;
+    ethDataArray.push(ethData);
+    // console.log("ethDataArray dentro de getEthPrice es:");
+    //console.log(ethDataArray);
 //    console.log("El valor crypto es:", valorCrypto);
-    console.log("El valor de ethData es:", ethData);
+//   console.log((i+1));
+console.log("El valor de ethData dentro de getEthPrice es:", ethData.quote.USD.price);
+console.log("la hora de obtencin del precio fue: ", ethData.date);
+//console.log(ethData);
+
+ethDataArray.forEach(element => {
+      console.log("dentro del GetEthPrice forEach element: " + ethDataArray.name + " vale " + element.quote.USD.price);
+     });
+
 }).catch(error => {
    console.error("Error al obtener el precio de ETH:", error);
 }); 
+//}
+
 
  
  // console.log("Pepe tenia un jarrito y un jarrito tenia a pepe, pepe se vio en el jarrito y el jarrito tenia a pepe");
- //console.log("el valor crypto es " + valorCrypto);
- console.log("el valor de ethData es " + ethData);
  
